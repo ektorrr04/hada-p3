78a830430053ee2620306c18af964f89d288cfb3
Your Name <you@example.com>
2026-03-26 12:53:10 +0100

    24439968F completada clase ENCategory

--- library/ENCategory.cs
+++ library/ENCategory.cs
@@ -8,5 +8,44 @@ namespace library
 {
     internal class ENCategory
     {
+
+        private int id;
+        private string name;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public ENCategory()
+        {
+            id = 0;
+            name = "";
+        }
+
+        public ENCategory(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public bool Read()
+        {
+            CADCategory cad = new CADCategory();
+            return cad.read(this);
+        }
+
+        public List<ENCategory> ReadAll()
+        {
+            CADCategory cad = new CADCategory();
+            return cad.readAll();
+        }
     }
 }
