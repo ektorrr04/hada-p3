a6107e6f7087651d676a2c080e115503e2bd5109
hecto <hecto@DESKTOP-BGCV6SK>
2026-03-27 19:15:04 +0100

    20884895K clase ENProduct añadida

---
+++ library/ENProduct.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace library
+{
+    public class ENProduct
+    {
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private int _category;
+        private DateTime _creationDate;
+
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+        public float Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public int Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+        public DateTime CreationDate
+        {
+            get { return _creationDate; }
+            set { _creationDate = value; }
+        }
+
+        public ENProduct() { }
+
+        public ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+            _code = code;
+            _name = name;
+            _amount = amount;
+            _price = price;
+            _category = category;
+            _creationDate = creationDate;
+        }
+
+        public bool Create()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.Create(this);
+        }
+
+        public bool Update()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.Update(this);
+        }
+
+        public bool Delete()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.Delete(this);
+        }
+
+        public bool Read()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.Read(this);
+        }
+
+        public bool ReadFirst()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.ReadFirst(this);
+        }
+
+        public bool ReadNext()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.ReadNext(this);
+        }
+
+        public bool ReadPrev()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.ReadPrev(this);
+        }
+    }
+}
